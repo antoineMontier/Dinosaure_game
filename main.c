1170a790353eacbe6a9b6997e684fe8e8c2fdb69
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-11-11 16:35:09 +0100

    Added color pallette

--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 #define FRAMES_PER_SECOND 40
 #define BUTTON_WIDTH 80
 #define BUTTON_HEIGHT 60
+#define PALETTE 3
 
 
 
@@ -24,6 +25,12 @@ typedef struct{
     double y;//center down of the bulb
     double r;//height of the bulb
 }bulb;
+
+typedef struct{
+    unsigned int r;//red 0-255
+    unsigned int g;//green 0-255
+    unsigned int b;//blue 0-255
+}Color;
 
 
 
@@ -36,35 +43,87 @@ void rect(SDL_Renderer* r, int x, int y, int height, int width, int filled);
 void circle(SDL_Renderer * r, int centreX, int centreY, int radius);
 void openSDL(int x, int y, int mode, SDL_Window**w, SDL_Renderer**r);
 void closeSDL(SDL_Window**w, SDL_Renderer**r);
-void background(SDL_Renderer* r, int red, int green, int blue);
-void drawLandscape(SDL_Renderer* r);
-void drawDino(SDL_Renderer* r, double x, double y);//x and y values relates to the left bottom corner of the "dino"
+void background(SDL_Renderer* r, Color*c, int p);
+void drawLandscape(SDL_Renderer* r, Color*c, int p);
+void drawDino(SDL_Renderer* r, double x, double y, Color*c, int p);//x and y values relates to the left bottom corner of the "dino"
 void updateDinoPosition(double*x, double*y, double*vy, double*ay);
-void drawBulbs(SDL_Renderer* r, bulb *bulb);
+void drawBulbs(SDL_Renderer* r, bulb *bulb, Color*c, int p);
 void moveBulbs(bulb *bulb, int tc);
 int distanceBeetweenWithOtherBulbsisLargerThan(bulb b, bulb*bb, unsigned int n);
-void superJumpBar(SDL_Renderer* r, int n);
+void superJumpBar(SDL_Renderer* r, int n, Color*c, int p);
 int died(bulb *bulbs, double x, double y);
 double dist(double x1, double y1, double x2, double y2);
-void printRestartButton(SDL_Renderer* r);
+void printRestartButton(SDL_Renderer* r, Color*c, int p);
 int rollover(int mx, int my, int x, int y, int w, int h);
-void restartGame(bulb* b, double *x, double *y, double *vy, double *ay, int *tc);
+void restartGame(bulb* b, double *x, double *y, double *vy, double *ay, int *tc, int *p);
 void jump(double*y, double*vy, int*sj);
 
 
 
+int main(int argc, char *args[]){//compile and execute with     gcc main.c -o main -lm $(sdl2-config --cflags --libs) && ./main
+
+    Color *colors = malloc(4*PALETTE*sizeof(Color));//store 9*4 colors that behave nicely 4 to 4
+
+    //first palette // blue theme
+
+    colors[0].r = 9;
+    colors[0].g = 43;
+    colors[0].b = 73;
+
+    colors[1].r = 8;
+    colors[1].g = 34;
+    colors[1].b = 58;
+
+    colors[2].r = 25;
+    colors[2].g = 78;
+    colors[2].b = 125;
+
+    colors[3].r = 43;
+    colors[3].g = 101;
+    colors[3].b = 153;
+
+    //second palette // green theme
+
+    colors[4].r = 43;
+    colors[4].g = 142;
+    colors[4].b = 0;
+
+    colors[5].r = 27;
+    colors[5].g = 88;
+    colors[5].b = 0;
+
+    colors[6].r = 43;
+    colors[6].g = 142;
+    colors[6].b = 0;
+
+    colors[7].r = 7;
+    colors[7].g = 23;
+    colors[7].b = 0;
+
+    //third palette // red theme
+
+    colors[8].r = 97;
+    colors[8].g = 0;
+    colors[8].b = 0;
+
+    colors[9].r = 114;
+    colors[9].g = 0;
+    colors[9].b = 0;
+
+    colors[10].r = 155;
+    colors[10].g = 0;
+    colors[10].b = 0;
+
+    colors[11].r = 182;
+    colors[11].g = 2;
+    colors[11].b = 2;
 
 
-
-
-
-
-
-
-int main(int argc, char *argv[]){//compile and execute with     gcc main.c -o main -lm $(sdl2-config --cflags --libs) && ./main
 
     SDL_Window *w;//open a window command
     SDL_Renderer *ren;//render creation
+
+    int palette = 0;
     double d_x, d_y, d_vy, d_ay;
 
     bulb* bulbs = malloc(BULBS_NUMBER*sizeof(bulb));//array for the bulbs
@@ -78,27 +137,26 @@ int main(int argc, char *argv[]){//compile and execute with     gcc main.c -o ma
     int survived = 0;
     int last_survived = 0;
 
-    restartGame(bulbs, &d_x, &d_y, &d_vy, &d_ay, &tick_count);
+    restartGame(bulbs, &d_x, &d_y, &d_vy, &d_ay, &tick_count, &palette);
 
 
     /*----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
     while(program_launched){//principal loop
         SDL_Event evt;
 
-            //printf("dino coordinates       y:%f\tvy:%f\tay:%f\n", d_y, d_vy, d_ay);
             if(!died(bulbs, d_x, d_y)){
                 moveBulbs(bulbs, tick_count);
                 updateDinoPosition(&d_x, &d_y, &d_vy, &d_ay);
 
-                background(ren, 255, 255, 255);
-                drawBulbs(ren, bulbs);
+                background(ren, colors, palette);
+                drawBulbs(ren, bulbs, colors, palette);
 
-                drawLandscape(ren);
+                drawLandscape(ren, colors, palette);
 
-                superJumpBar(ren, super_jump);
+                superJumpBar(ren, super_jump, colors, palette);
 
 
-                drawDino(ren, d_x, d_y);
+                drawDino(ren, d_x, d_y, colors, palette);
 
                 /*color(ren, 255, 0, 0, 255);
                 mark(ren, 80, 160, 5);*/
@@ -110,7 +168,7 @@ int main(int argc, char *argv[]){//compile and execute with     gcc main.c -o ma
                     last_survived = survived;
                     survived = 0;
                 }
-                printRestartButton(ren);
+                printRestartButton(ren, colors, palette);
             }
                 SDL_RenderPresent(ren);//refresh the render
 
@@ -126,14 +184,13 @@ int main(int argc, char *argv[]){//compile and execute with     gcc main.c -o ma
                 case SDL_KEYDOWN:                   //SDL_KEYDOWN : hold a key            SDL_KEYUP : release a key
                     switch (evt.key.keysym.sym){//returns the key ('0' ; 'e' ; 'SPACE'...)
 
-                        case SDLK_v:
-                            jump(&d_y, &d_vy, &super_jump);
-                            break;
-
                         case SDLK_ESCAPE:
                             program_launched = SDL_FALSE;//escape the program by pressing esc
                             break;
 
+                        case SDLK_SPACE:
+                            jump(&d_y, &d_vy, &super_jump);
+                            break;
 
                         default:
                             break;
@@ -143,13 +200,13 @@ int main(int argc, char *argv[]){//compile and execute with     gcc main.c -o ma
                 case SDL_MOUSEBUTTONDOWN:
                     if(!survived){//wait for the rollover only if the game is not running
                         int k = rollover(evt.button.x, evt.button.y, WIDTH/2 - BUTTON_WIDTH/2, HEIGHT/2 - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT);
-                        if(k)
-                            restartGame(bulbs, &d_x, &d_y, &d_vy, &d_ay, &tick_count);
+                        if(k){
+                            restartGame(bulbs, &d_x, &d_y, &d_vy, &d_ay, &tick_count, &palette);
+                            last_survived = SDL_GetTicks();
+                        }
+
                     }else   
                         jump(&d_y, &d_vy, &super_jump);
-
-
-
 
                 default:
                     break; 
@@ -159,11 +216,6 @@ int main(int argc, char *argv[]){//compile and execute with     gcc main.c -o ma
 
         
 
-    
-        
-
-
-    
         tick_count++;
         if(super_jump)
             super_jump--;
@@ -177,7 +229,7 @@ int main(int argc, char *argv[]){//compile and execute with     gcc main.c -o ma
     /*SDL_RenderPresent(ren);//refresh the render
     SDL_Delay(5000);//waiting delay, in ms*/
     /*----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
+    free(colors);
     free(bulbs);
     closeSDL(&w, &ren);
     printf("closed successfully !\n");
@@ -282,23 +334,23 @@ void closeSDL(SDL_Window**w, SDL_Renderer**r){
     SDL_Quit();
 }
 
-void background(SDL_Renderer* r, int red, int green, int blue){
-    color(r, red, green, blue, 255);
+void background(SDL_Renderer* r, Color*c, int p){
+    color(r, c[4*p+3].r, c[4*p+3].g, c[4*p+3].b, 255);
     rect(r, 0, 0, WIDTH, HEIGHT, 1);
 }
 
-void drawLandscape(SDL_Renderer* r){
+void drawLandscape(SDL_Renderer* r, Color*c, int p){
     int alpha = 255;
     for(int y = 3*HEIGHT/4 ; y < HEIGHT && alpha > 0; y ++){
-        color(r, 255-alpha, 255-alpha, 255-alpha, alpha);
+        color(r, c[4*p+1].r, c[4*p+1].g, c[4*p+1].b, alpha);
         line(r, 0, y, WIDTH, y);
         alpha -= 1;
     }
     
 }
 
-void drawDino(SDL_Renderer* r, double x, double y){
-    color(r, 0, 0, 0, 255);
+void drawDino(SDL_Renderer* r, double x, double y, Color*c, int p){
+    color(r, c[4*p].r, c[4*p].g, c[4*p].b, 255);
     rect(r, x, y - DINO_HEIGHT, DINO_WIDTH , DINO_HEIGHT, 1);
 }
 
@@ -316,8 +368,8 @@ void updateDinoPosition(double*x, double*y, double*vy, double*ay){
 
 }
 
-void drawBulbs(SDL_Renderer* r, bulb *bulb){//!\ to draw the bulb before the background, otherwise the bulb will be a simple circle
-    color(r, 50, 50, 50, 255);
+void drawBulbs(SDL_Renderer* r, bulb *bulb, Color*c, int p){//!\ to draw the bulb before the background, otherwise the bulb will be a simple circle
+    color(r, c[4*p+1].r, c[4*p+1].g, c[4*p+1].b, 255);
     for(int b = 0 ; b < BULBS_NUMBER ; b ++){
         for(int i = bulb[b].r ; i >= bulb[b].r - 5 ; i--){//thicken the border of the bulb
             circle(r, bulb[b].x, bulb[b].y, i);
@@ -361,7 +413,7 @@ void moveBulbs(bulb *bulbs, int tc){//to upgrade : bulbs will go faster and fast
                 bulbs[i].r = DINO_HEIGHT/2 + rand() % DINO_HEIGHT/2;
                 bulbs[i].x = WIDTH + rand() % WIDTH;                               //distance might be enought
                 iteration_counter++;
-            } while (!distanceBeetweenWithOtherBulbsisLargerThan(bulbs[i], bulbs, DINO_WIDTH*3) && iteration_counter < 100);
+            } while(!distanceBeetweenWithOtherBulbsisLargerThan(bulbs[i], bulbs, DINO_WIDTH*4) && iteration_counter < 100);
             
         }
     }
@@ -377,22 +429,19 @@ void moveBulbs(bulb *bulbs, int tc){//to upgrade : bulbs will go faster and fast
 
 }
 
-void superJumpBar(SDL_Renderer* r, int n){
+void superJumpBar(SDL_Renderer* r, int n, Color*c, int p){
     //origin at x = 50 y = 150
     int x = 50;
     double pace = SUPER_JUMP_TIMER/100.0; // 100 is the size of the bar;
-    color(r, 200, 200, 200, 255);//background grey
+    color(r, c[4*p+3].r, c[4*p+3].g, c[4*p+3].b, 255);//background
 
     for(int y = 140 ; y >= 40 ; y--){
         for(int x = 47 ; x <= 53 ; x++)
             point(r, x, y);
     }
 
-    color(r, 0, 255, 68, 255);//green
+    color(r, c[4*p+1].r, c[4*p+1].g, c[4*p+1].b, 255);//bar color
 
-    if(n == 0){//changes bar color if energy full
-        color(r, 0, 153, 51, 255);
-    }
 
     for(int y = 40 ; y <= 140; y++){
         for(int x = 47 ; x <= 53 ; x++)
@@ -422,8 +471,8 @@ int died(bulb *bulbs, double x, double y){//there's a 1 pixel delta
     return 0;//no died
 }
 
-void printRestartButton(SDL_Renderer* r){
-    color(r, 100, 100, 100, 150);
+void printRestartButton(SDL_Renderer* r, Color*c, int p){
+    color(r, c[4*p].r, c[4*p].g + 30, c[4*p].b, 150);
     rect(r, WIDTH/2 - BUTTON_WIDTH/2, HEIGHT/2 - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT, 1);
     color(r, 20, 20, 20, 255);
     rect(r, WIDTH/2 - BUTTON_WIDTH/2, HEIGHT/2 - BUTTON_HEIGHT/2, BUTTON_WIDTH, BUTTON_HEIGHT, 0);
@@ -436,7 +485,7 @@ int rollover(int mx, int my, int x, int y, int w, int h){
     return 0;//false
 }
 
-void restartGame(bulb* b, double *x, double *y, double *vy, double *ay, int *tc){
+void restartGame(bulb* b, double *x, double *y, double *vy, double *ay, int *tc, int*p){
     for(int i = 0 ; i < BULBS_NUMBER ; i++){ //initialise like this in order to be regenerated randomly
         b[i].x = -100 + i*10;
         b[i].y = 3*HEIGHT/4;
@@ -448,6 +497,11 @@ void restartGame(bulb* b, double *x, double *y, double *vy, double *ay, int *tc)
     *vy = 0;//speed
     *ay = ACCELERATION/100.0;//acceleration
     *tc = 0; //reset ticks
+    int k = *p;
+    do{
+        *p = rand() % PALETTE;
+    } while (*p == k);
+    
 }
 
 void jump(double*y, double*vy, int*sj){
